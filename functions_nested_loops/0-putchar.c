0a979277e76aaa22f6e3994ace0070aff0e0f2f9
Darian94 <dariangra@gmail.com>
2022-09-30 09:25:33 -0700

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -5,5 +5,16 @@
  * Return: 0 (success)
  */
 
-int _putchar(char c)
-	_putchar("_putchar")
+int main(void)
+{
+	_putchar ('_');
+	_putchar ('p');
+	_putchar ('u');
+	_putchar ('t');
+	_putchar ('c');
+	_putchar ('h');
+	_putchar ('a');
+	_putchar ('r');
+	_putchar ('\n');
+	return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,8 @@
+#ifndef main_h
+#define main_h
 #include <unistd.h>
 #include <stdio.h>
 
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
+
+#endif
