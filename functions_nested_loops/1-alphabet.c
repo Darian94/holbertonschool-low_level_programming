9f80c04892edc40761f86111b7434a8bd18057d6
Darian94 <dariangra@gmail.com>
2022-10-01 13:03:05 -0700

    alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,11 +1,16 @@
-#include "main"
+#include "main.h"
 
-int main(void)
+/**
+ * print_alphabet - librery
+ * Return: 0 (success)
+ */
 
+void print_alphabet(void)
+{
 	char n;
 
-	for (n = 97 ; n <= 122 ; ++n)
-	_putchar (n);
+	for (n = 'a' ; n <= 'z' ; ++n)
+		_putchar (n);
 	_putchar ('\n');
-	return (0);
- 
+	
+}
