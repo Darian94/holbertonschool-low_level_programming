a0938e9513a1cc174fd8990e23c60d4a9baf74c0
Darian94 <dariangra@gmail.com>
2022-10-03 10:22:08 -0700

    table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - check the code
+ *
+ * Return: Always 0.
+ */
+
+void times_table(void)
+{
+	int a, b;
+
+	for (a = 0 ; a <= 9 ; ++a)
+	{
+		for (b = 0 ; b <= 9 ; ++b)
+		{
+			if ((a * b) >= 10)
+			{
+				_putchar(' ');
+				_putchar((a * b) / 10 + '0');
+				_putchar((a * b) % 10 + '0');
+			}
+			else if ((a * b) < 10 && b != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((a * b) + '0');
+			}
+			if (b < 9)
+			{
+				_putchar(',');
+			}
+			else
+				_putchar('\n');
+		}
+	}
+}
+
