2a80c5d9dd531b59228bc4f048c85e753474cdbe
Darian94 <dariangra@gmail.com>
2022-10-10 11:36:20 -0700

    task 0.strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -12,9 +12,9 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int n;
 
-	for(i = 0 ; dest[i] != '\0' ; i++)
+	for (i = 0 ; dest[i] != '\0' ; i++)
 		;
-	for(n = 0 ; src[n] != '\0' ; n++)
+	for (n = 0 ; src[n] != '\0' ; n++)
 	{
 		dest[i] = src[n];
 		i++;
