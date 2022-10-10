10f194ba138bc32f798721a9effa334b9dde178a
Darian94 <dariangra@gmail.com>
2022-10-10 07:28:11 -0700

    print half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - check the code
+ * @str: pointer
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	int aux;
+
+	for (i = 0 ; str[i] != '\0'; i++)
+		;
+	if (i % 2 == 0)
+	{
+		for (aux = i / 2; aux <= i; aux++)
+		{
+			_putchar(str[aux]);
+		}
+	}
+	else
+	{
+		i = i - 1;
+		for (aux = i / 2; aux <= i; aux++)
+		{
+			_putchar(str[aux]);
+		}
+	}
+	_putchar(10);
+}
