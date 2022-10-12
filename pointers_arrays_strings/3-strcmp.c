f50ff1fa2e93741652362073707354ffb8115f8f
Darian94 <dariangra@gmail.com>
2022-10-12 07:52:02 -0700

    compares two strings

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: pointer
+ * @s2: pointer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for(i=0 ; s1[i] - s2[i] == 0 && s1[i] != '\0' ; i++)
+		;
+	return (s1[i] - s2[i]);
+}	
