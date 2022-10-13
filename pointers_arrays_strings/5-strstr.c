55900059d7a72ab0e0a3de072f374d44ad31e03f
Darian94 <dariangra@gmail.com>
2022-10-13 14:44:26 -0700

    function that locates a substring

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -10,27 +10,34 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-  int i;
-  int k = 0;
-  int l = 0;
-  if (needle[0] == '\0')
-    return (haystack);
-  for (i = 0; haystack[i]; i++)
-    {
-      if (haystack[i] == needle[0])
+	int i;
+	int k = 0;
+	int l = 0;
+
+	if (needle[0] == '\0')
 	{
-	  while (haystack[i + k] == needle[k])
-	    {
-	      k++;
-	      l++;
-	    }
-	  if (l == _strlen(needle))
-	    break;
+		return (haystack);
 	}
-    }
-  if (l == _strlen(needle))
-    return (haystack + i);
-  return (NULL);
+	for (i = 0; haystack[i]; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			while (haystack[i + k] == needle[k])
+			{
+				k++;
+				l++;
+			}
+			if (l == _strlen(needle))
+			{
+				break;
+			}
+		}
+	if (l == _strlen(needle))
+	{
+		return (haystack + i);
+	}
+	}
+	return (NULL);
 }
 
 /**
@@ -40,8 +47,11 @@ char *_strstr(char *haystack, char *needle)
  */
 int _strlen(char *s)
 {
-  int i;
-  while (*(s + i))
-    i++;
-  return (i);
+	int i;
+
+	while (*(s + i))
+	{
+		i++;
+	}
+	return (i);
 }
