96670e93e013befcce446cfead5746eca6f6e9fe
Darian94 <dariangra@gmail.com>
2022-10-13 07:27:23 -0700

    encodes a string

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *leet - function that encodes a string
+ * @s: pointer
+ *
+ */
+
+char *leet(char *s)
+{
+	int i;
+	int x;
+
+	char let[] = {'a','A','e','E','O','o','t','T','l','L'};
+	char num[] = {'4','4','3','3','0','0','7','7','1','1'};
+
+	for (i = 0 ; s[i] !='\0' ; i++)
+	{
+		for (x = 0 ; x <= 9 ; x++)
+		{
+			if (s[i] == let[x])
+			{
+				s[i] = num[x];
+			}
+		}
+	}
+	return(s);
+}
