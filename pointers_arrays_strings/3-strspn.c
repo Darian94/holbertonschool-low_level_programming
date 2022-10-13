e0d1362a58f990a955f1a710049201597a21af95
Darian94 <dariangra@gmail.com>
2022-10-13 12:58:56 -0700

    length of a prefix substring

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * int _strspn - length of a prefix substring
+ * @s: string
+ * @accept: string
+ * Return: cont
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int x;
+	unsigned int cont = 0;
+
+	for (i = 0 ; s[i] != ' ' ; i++)
+	{
+		for (x = 0 ; accept[x] != '\0' ; x++)
+		{
+			if (s[i] == accept[x])
+			{
+				cont++;
+			}
+		}
+	}
+	return (cont);
+}
+
+
+
