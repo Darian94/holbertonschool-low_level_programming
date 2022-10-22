d16cc62ddd89261d4560191817e3c41f1ee43666
Darian94 <dariangra@gmail.com>
2022-10-22 05:53:04 -0700

    2 dimensional

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * alloc_grid - function that frees a 2 dimensional grid
+ * @width: width size value
+ * @height: height size value
+ * Return: NULL, point
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int n, i;
+	int **point;
+
+	if (width < 1 || height < 1)
+	{
+		return (NULL);
+	}
+
+	point = malloc(sizeof(*point) * height);
+
+	if (point == NULL)
+	{
+		return (NULL);
+	}
+
+	for (n = 0 ; n < height ; n++)
+	{
+		point[n] = malloc(sizeof(int) * width);
+
+		if (point[n] == NULL)
+		{
+			while (i >= 0)
+		{
+			i--;
+			free(point[n]);
+		}
+		free(point);
+		return (NULL);
+		}
+		else
+		{
+			for (i = 0 ; point[n][i] ; i++)
+			{
+				point[n][i] = 0;
+			}
+		}
+	}
+	return (point);
+}
+
+
