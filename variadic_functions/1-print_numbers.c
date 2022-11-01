28522d1300b78bc657307e5a0c8236dcd0d40b75
Darian94 <dariangra@gmail.com>
2022-11-01 12:46:54 -0700

    prints numbers

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - fuction that print numbers
+ * @separator: separator of numbers
+ * @n: numbers of numbers to print
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list print;
+
+	unsigned int x, numbers;
+
+	numbers = 0;
+
+	if (separator != NULL)
+	{
+		va_start(print, n);
+		for (x = 0 ; x < n ; x++)
+		{
+			numbers = va_arg(print, int);
+			if (x != 0)
+				printf("%s", separator);
+			printf("%d", numbers);
+		}
+	printf("\n");
+	va_end(print);
+	}
+}
+
