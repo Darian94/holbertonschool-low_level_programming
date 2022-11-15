16a4e2f56da5d7bf836482d4bf7444f93217157b
Darian94 <dariangra@gmail.com>
2022-11-15 10:24:18 -0800

    function that prints anything

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -17,35 +17,29 @@ void print_all(const char * const format, ...)
 	{
 		p = strlen(format);
 		va_start(arg, format);
-
 		while (format[n] != '\0')
 		{
 			check = 0;
-
 			switch (format[n])
 			{
 				case 'c':
 					printf("%c", va_arg(arg, int));
 					check = 1;
 					break;
-
 				case 'i':
 					printf("%i", va_arg(arg, int));
 					check = 1;
 					break;
-
 				case 'f':
 					printf("%f", va_arg(arg, double));
 					check = 1;
 					break;
-
 				case 's':
 					s = va_arg(arg, char *);
 					printf("%s", s == 0 ? "(nil)" : s);
 					check = 1;
 					break;
-					
-			}	
+			}
 			if (n < p - 1 && check == 1)
 				 printf(", ");
 			n++;
