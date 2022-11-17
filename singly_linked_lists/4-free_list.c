818561a91f03f7bf12dab67eef974f739524d4af
Darian94 <dariangra@gmail.com>
2022-11-17 14:38:17 -0800

    frees a list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * free_list - free a list
+ * @head: pointer the node
+ */
+
+void free_list(list_t *head)
+{
+	while(head)
+	{
+		free(head->next);	
+		free(head);
+	}
+}
