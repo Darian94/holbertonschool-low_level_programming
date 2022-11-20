0ea1d7f6bf78bfc4d4fc92324c00f329120c37cd
Darian94 <dariangra@gmail.com>
2022-11-20 14:52:23 -0800

    more singly linked lists

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/*
+ * print_listint - function that prints the elements of a node
+ * @h: pointer to the first struct of the list
+ * Return: i
+ */
+
+size_t print_listint(const listint_t *h);
+{
+	int i = 0;
+	const listint_t *list;
+
+	if (h == NULL)
+		return(i);
+
+	*list = h;
+	while(list != NULL)
+		{
+			printf("%d/n", list->n);
+			i++
+			lisy = list->next;
+		}
+	return (i);
+}
+
