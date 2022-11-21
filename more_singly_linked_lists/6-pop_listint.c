5e30113c2586d53ae924a95407fe4252dfdc47e7
Darian94 <dariangra@gmail.com>
2022-11-20 18:41:39 -0800

    delete the head node of a list

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * pop_listint - delete a first node
+ * @head: pointer to the first node
+ * Return: new start
+ */                                                                             int pop_listint(listint_t **head)
+{
+	listint_t *aux;
+	if (head != NULL)
+	{
+		aux = head;
+		head = head->next;
+		free(aux);
+	}
+	return (head);
+}
