9ce333e9abcd8bb69621c7f6557da9b7ff1f9a9a
Darian94 <dariangra@gmail.com>
2022-11-20 19:08:50 -0800

    free a list

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -9,9 +9,7 @@ size_t print_listint(const listint_t *h)
 {
 	int i = 0;
 	const listint_t *list;
-
 	list = h;
-
 	if (h == NULL)
 	{
 		return (i);
--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -9,7 +9,6 @@ void free_listint2(listint_t **head)
 {
 	if (head)
 	{
-		free(head->next);
 		free_listint(*head);
 		*head = NULL;
 	}
