ccb6a1bcf9fbe03803203f2c9a3fe6238c93fda1
Darian94 <dariangra@gmail.com>
2022-11-22 05:38:38 -0800

    prints all the elements of a list

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -10,10 +10,12 @@ size_t print_listint(const listint_t *h)
 	int i = 0;
 	const listint_t *list;
 	list = h;
+
 	if (h == NULL)
 	{
 		return (i);
 	}
+
 	while (list != NULL)
 		{
 			printf("%d\n", list->n);
