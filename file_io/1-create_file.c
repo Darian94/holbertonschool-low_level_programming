9824364bf2baee0bef199f664be82fc0fc417acb
Darian94 <dariangra@gmail.com>
2022-11-26 11:12:24 -0800

    function that crates a file and write iin it

---
+++ file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * create_file - create a file and writes content in it
+ * @filename: name of the file
+ * @text_content: text to write in the file
+ * Return: 1 on success -1 if it fails
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int len, fd, w;
+
+	if (text_content == NULL)
+		return (-1);
+	len = _strlen(text_content);
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_RDWR, 0600);
+	if (fd == -1)
+		return (0);
+
+	w = write(fd, text_content, len);
+	if (w == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
+/**
+ * _strlen - function than retunr the length of a text string
+ * @s: pointer to the adress of the string
+ * Return: all elements of the string
+ */
+
+int _strlen(char *s)
+{
+	int n = 0;
+
+	while (s[n] != '\0')
+		n++;
+	return (n);
+}
