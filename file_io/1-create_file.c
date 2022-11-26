abb8246808256302be5e46c6d3f03571b2aeffd0
Darian94 <dariangra@gmail.com>
2022-11-26 11:25:58 -0800

    function that crates a file and write iin it

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_RDWR, 0600);
 	if (fd == -1)
-		return (0);
+		return (-1);
 
 	w = write(fd, text_content, len);
 	if (w == -1)
