d89d020cacb254013c3394ffcf80201d6ef5a91f
Darian94 <dariangra@gmail.com>
2022-11-26 11:16:53 -0800

    file header

--- file_io/main.h
+++ file_io/main.h
@@ -2,9 +2,12 @@
 #define main_h
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int _strlen(char *s);
 
 #endif
