ec06776a47814f5902bd819cfb76246e685f9e25
Darian94 <dariangra@gmail.com>
2022-11-27 12:33:51 -0800

    program than copies the countent of a file to another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -5,6 +5,7 @@
  * @argc: argument counter
  * @argv: argument vector
  * Return: 1 is success and -1 on failure
+ * In 22 and 30, i put 2 instead of STDERR_FILENO because i pass caractr permis
  */
 
 int main(int argc, char *argv[])
@@ -13,54 +14,28 @@ int main(int argc, char *argv[])
 	char *buffer[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	if (!argv[1])
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file \n");
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't read from file \n"), exit(98);
 	fd_old = open(argv[1], O_RDONLY);
 	if (fd_old == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	fd_new = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (fd_new == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	r = read(fd_old, buffer, 1024);
 	while (r != 0)
 	{
 		if (r == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
+			dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
 		w = write(fd_new, buffer, r);
 		if (w == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-			exit(99);
-		}
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 		r = read(fd_old, buffer, 1024);
 	}
 	if ((close(fd_old)) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_old);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_old), exit(100);
 	if ((close(fd_new)) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_new);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_new), exit(100);
 	return (0);
 }
-
-
