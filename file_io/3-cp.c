047c71f0a60c9dd196eec469749ae9acfa08e321
Darian94 <dariangra@gmail.com>
2022-11-27 11:26:37 -0800

    program than copies the countent of a file to another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -14,18 +14,18 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(2,"Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	if (!argv[1])
 	{
-		dprintf(2,"Error: Can't read from file_from\n");
+		dprintf(STDERR_FILENO, "Error: Can't read from file_from\n");
 		exit(98);
 	}
 	fd_old = open(argv[1], O_RDONLY);
 	if (fd_old == -1)
 	{
-		dprintf(2,"Error: Can't read from file_from %s\n",argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file_from %s\n",argv[1]);
 		exit(98);
 	}
 	fd_new = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
@@ -35,12 +35,12 @@ int main(int argc, char *argv[])
 		w = write(fd_new, buffer, r);
 		if (r == -1)
 		{
-			dprintf(2, "Error: Can't read from file_from %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file_from %s\n", argv[1]);
 			exit(98);
 		}
 		if (w == -1)
 		{
-			dprintf(2, "Error: Can't write to file_to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to file_to %s\n", argv[2]);
 			exit(99);
 		}
 		r = read(fd_old, buffer, 1024);
@@ -48,13 +48,13 @@ int main(int argc, char *argv[])
 	close(fd_old);
 	if ((close(fd_old)) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_old);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_old);
 		exit(100);
 	}
 	close(fd_new);
 	if ((close(fd_new)) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_new);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_new);
 		exit(100);
 	}
 	return (0);
