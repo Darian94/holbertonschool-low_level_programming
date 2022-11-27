586765df18ee4e4234030902701c4c9b70201473
Darian94 <dariangra@gmail.com>
2022-11-27 12:50:46 -0800

    program than copies the countent of a file to another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -19,7 +19,10 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file \n"), exit(98);
 	fd_old = open(argv[1], O_RDONLY);
 	if (fd_old == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	fd_new = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (fd_new == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
@@ -27,7 +30,10 @@ int main(int argc, char *argv[])
 	while (r != 0)
 	{
 		if (r == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
 		w = write(fd_new, buffer, r);
 		if (w == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
