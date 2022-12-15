c1adae32e61fb356084db26c496f046ce0f442a4
Darian <113607653+DarianGrabino@users.noreply.github.com>
2022-12-15 01:00:54 -0300

    Update 0-reset_to_98.c
    fix

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 - check the code
- * @n: updates the valuei
+ * reset_to_98 - function that changes what a pointer points to
+ * @n: updates the value
  * Return: void
  */
 
