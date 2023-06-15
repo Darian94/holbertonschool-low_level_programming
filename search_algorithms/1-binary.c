06d7d9b86f1c815bd0556f56f8147071d336a8cc
DarianGrabino <dariangra@gmail.com>
2023-06-14 17:15:21 -0700

    search binary

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -3,37 +3,55 @@
 #include <string.h>
 
 /**
- * binary_search - searches for a value in a sorted array of integers
- * @array: The array to be sorted.
- * @size: The size of the array to be sorted.
- * @value:
- * return: -1 if not found
-*/
+  * print_array - Prints the array from left to right
+  * @array: Array to print
+  * @left: First index to print.
+  * @right: Last index to print.
+  *
+  */
+
+void print_array(int *array, size_t left, size_t right)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = left; i <= right; i++)
+	{
+		printf("%d", array[i]);
+		if (i != right)
+			printf(", ");
+		else
+			printf("\n");
+	}
+}
+
+/**
+  * binary_search - searches value in sorted array of ints using Binary search
+  * @array: Pointer to the first element of the array to seach in
+  * @size: Number of elements in array
+  * @value: Value to search
+  *
+  * Return: The index where the value is located, -1 otherwise
+  */
 
 int binary_search(int *array, size_t size, int value)
 {
-    if (array == NULL)
-        return (-1);
+	size_t left = 0, right = size - 1, middle;
 
-    int left = 0;
-    int right = size - 1;
-
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-
-        printf("Searching in subarray: ");
-        for (int i = left; i <= right; i++) {
-            printf("%d ", array[i]);
-        }
-        printf("\n");
-
-        if (array[mid] == value)
-            return mid;
-        else if (array[mid] < value)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
-
-    return (-1);
+	if (!array)
+		return (-1);
+	print_array(array, left, right);
+	while (left <= right)
+	{
+		middle = (left + right) / 2;
+		if (array[middle] < value)
+			left = middle + 1;
+		else if (array[middle] > value)
+			right = middle - 1;
+		else
+			return (middle);
+		if (left <= right)
+			print_array(array, left, right);
+	}
+	return (-1);
 }
